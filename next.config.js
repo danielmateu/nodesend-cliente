6c493b96f212610483ed84fd9e7bb61cb451ae20
Daniel Mateu <danielmateu86@gmail.com>
2023-02-07 19:40:01 +0100

    enougth for today...

--- next.config.js
+++ next.config.js
@@ -7,6 +7,6 @@ module.exports = {
   nextConfig,
   env: {
     backendURL: 'https://stormy-sea-94958.herokuapp.com/',
-    frontEndURL: 'https://nuk-send.vercel.app/'
+    frontEndURL: 'https://nuk-send.vercel.app'
   }
 }
