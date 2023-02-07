c97dbb1aadebe0e255c595cb9017551511d57562
Daniel Mateu <danielmateu86@gmail.com>
2023-02-07 16:49:13 +0100

    mods...

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   env: {
-    backendURL: 'https://nuk-send-node.herokuapp.com',
+    backendURL: 'https://obscure-bayou-61065.herokuapp.com',
     frontEndURL: 'https://nuk-send.vercel.app'
   }
 }
